fb8068242bf691e2a5812f578715b1d5b4a04626
Divyansh8843 <divyanshagrawal8843@gmail.com>
2025-06-15 00:19:04 +0530

    changes done

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import UserContext from "./context/userContext.jsx";
+import UserContext from "./context/UserContext.jsx";
 import CaptainContext from "./context/CaptainContext.jsx";
 import SocketProvider from "./context/SocketContext.jsx";
 createRoot(document.getElementById("root")).render(
